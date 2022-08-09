2e3be0ca340304f9fc16f84cbda1be13dfafe4c9
Camila Da Silva Figueira <camila.figueira@inmetrics.com.br>
2022-08-09 17:46:37 -0300

    config fix

--- api_clientes/config.py
+++ api_clientes/config.py
@@ -2,10 +2,10 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL(app)
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'camila123'
-app.config['MYSQL_DATABASE_DB'] = 'apiflask'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_DB'] = 'APIDB'
+app.config['MYSQL_DATABASE_HOST'] = 'db-camila.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 
 
 mysql.init_app(app)
--- api_inventario/config.py
+++ api_inventario/config.py
@@ -2,11 +2,12 @@ from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL(app)
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'camila123'
-app.config['MYSQL_DATABASE_DB'] = 'inventory'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_DB'] = 'APIDB'
+app.config['MYSQL_DATABASE_HOST'] = 'db-camila.cxycaymkd24m.us-east-1.rds.amazonaws.com'
 
-debug = True
+
 mysql.init_app(app)
 
+debug = True
