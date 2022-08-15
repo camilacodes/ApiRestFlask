d1fa566aec992728cb3c009bb6e8e1a429949582
Camila Da Silva Figueira <camila.figueira@inmetrics.com.br>
2022-08-15 12:10:45 -0300

    changing ports

--- api_clientes/config.py
+++ api_clientes/config.py
@@ -5,7 +5,7 @@ mysql = MySQL(app)
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'camila123'
 app.config['MYSQL_DATABASE_DB'] = 'APIDB'
-app.config['MYSQL_DATABASE_HOST'] = 'db-camila.cxycaymkd24m.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_HOST'] = 'db-camila.ckatnedhbqjh.us-east-2.rds.amazonaws.com'
 
 
 mysql.init_app(app)
