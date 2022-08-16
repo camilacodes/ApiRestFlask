b38e096140e5f01eea422f914fab8e07b1b6fd2e
Camila Da Silva Figueira <camila.figueira@inmetrics.com.br>
2022-08-16 13:37:41 -0300

    changing config

--- api_inventario/config.py
+++ api_inventario/config.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 mysql = MySQL(app)
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'camila123'
-app.config['MYSQL_DATABASE_DB'] = 'APIDB'
+app.config['MYSQL_DATABASE_DB'] = 'inventory'
 app.config['MYSQL_DATABASE_HOST'] = 'db-camila.ckatnedhbqjh.us-east-2.rds.amazonaws.com'
 
 
