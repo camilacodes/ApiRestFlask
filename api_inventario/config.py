14b782693890d10415885e4e55d671c8a80e5045
Camila Figueira <66393807+camilacodes@users.noreply.github.com>
2022-09-06 12:19:07 -0300

    Update config.py

--- api_inventario/config.py
+++ api_inventario/config.py
@@ -1,11 +1,10 @@
 from app import app
 from flaskext.mysql import MySQL
-
 mysql = MySQL(app)
-app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'camila123'
-app.config['MYSQL_DATABASE_DB'] = 'inventory'
-app.config['MYSQL_DATABASE_HOST'] = 'db-camila.ckatnedhbqjh.us-east-2.rds.amazonaws.com'
+app.config['MYSQL_DATABASE_USER'] = 
+app.config['MYSQL_DATABASE_PASSWORD'] = 
+app.config['MYSQL_DATABASE_DB'] = 
+app.config['MYSQL_DATABASE_HOST'] = 
 
 
 mysql.init_app(app)
